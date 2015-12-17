450ace62982a9a57d5b2084939a6e2dc78a241a1
killpack <killpackoff@gmail.com>
2015-12-17 17:35:17 +0300

    refs #8564 Create Java InnoHelper

---
+++ java-inno-helper/src/main/java/com/innometrics/integrationapp/model/PofileStreamMessage.java
@@ -0,0 +1,11 @@
+package com.innometrics.integrationapp.model;
+
+import com.google.gson.JsonElement;
+
+/**
+ * Created by killpack on 17.12.15.
+ */
+public class PofileStreamMessage {
+    Profile profile;
+    JsonElement meta;
+}
--- mapping/src/main/java/com/innometrics/integrationapp/test.java
+++
@@ -1,7 +0,0 @@
-package com.innometrics.integrationapp;
-
-/**
- * Created by kill_pack on 28.11.2015.
- */
-public class test {
-}
